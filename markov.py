b79e8ee045d70476e287372f02d25d48973851a7
caiobrighenti <caio.brig@gmail.com>
2019-07-12 13:35:36 -0400

    Implement markov chain

--- markov.py
+++ markov.py
@@ -1,1 +1,19 @@
 import markovify
+import os
+#os.getcwd()
+#os.chdir("C:/Users/Caio Laptop/Documents/Repositories/campo-blotter")
+
+# Get raw text as string.
+with open("blotter.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+# Print five randomly-generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
+# Print three randomly-generated sentences of no more than 280 characters
+for i in range(3):
+    print(text_model.make_short_sentence(660))
